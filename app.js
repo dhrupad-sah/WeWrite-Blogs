e1ae89055f8964d7a0cd55decc82a281456d8538
Dhrupad <thedhrupad@gmail.com>
2023-01-29 17:19:22 +0530

    made category routes

--- app.js
+++ app.js
@@ -7,25 +7,27 @@ const mongoose = require("mongoose");
 const app = express();
 
 const composeRouter = require("./routes/compose");
-const blogsRouter = require("./routes/blogs")
+const blogsRouter = require("./routes/blogs");
 
 const Blogs = require("./schemas/blogs");
 const Users = require("./schemas/users");
 
 app.use(express.static("public"));
-app.set("view engine","ejs");
-app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose
+  .connect(process.env.MONGO_URL)
   .then(() => console.log("DB connection successfull"))
-  .catch((err) => { console.log(err) });
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.get("/", (req,res)=>
-{
-    res.render("home");
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
 app.use("/compose", composeRouter);
 app.use("/blogs", blogsRouter);
 
-app.listen(3000, ()=> console.log("Connected to local host"));
+app.listen(3000, () => console.log("Connected to local host"));
--- routes/blogs.js
+++ routes/blogs.js
@@ -5,24 +5,109 @@ const router = new express.Router();
 const Blogs = require("../schemas/blogs");
 const Users = require("../schemas/users");
 
-router.get("/all", (req,res)=>
-{
-    Blogs.find({}, (e,docs)=>
-    {
-        if(e)
-        {
-            res.send(e);
-        }
-        else
-        {
-            res.render("blogs", {docs: docs});
-        }
-    })
-})
+// router.get("/all", (req,res)=>
+// {
+//     Blogs.find({}, (e,docs)=>
+//     {
+//         if(e)
+//         {
+//             res.send(e);
+//         }
+//         else
+//         {
+//             res.render("blogs", {docs: docs});
+//         }
+//     })
+// })
 
-router.get("/politics", (req,res)=>
-{
+router.get("/politics", (req, res) => {
+  Blogs.find({ category: "Politics" }, (e, docs) => {
+    if (e) {
+      res.send(e);
+    } else {
+      res.render("blogs", { docs: docs });
+    }
+  });
+});
 
-})
+router.get("/lifestyle", (req, res) => {
+  Blogs.find({ category: "Lifestyle" }, (e, docs) => {
+    if (e) {
+      res.send(e);
+    } else {
+      res.render("blogs", { docs: docs });
+    }
+  });
+});
+
+router.get("/fashion", (req, res) => {
+  Blogs.find({ category: "Fashion" }, (e, docs) => {
+    if (e) {
+      res.send(e);
+    } else {
+      res.render("blogs", { docs: docs });
+    }
+  });
+});
+
+router.get("/food", (req, res) => {
+  Blogs.find({ category: "Food" }, (e, docs) => {
+    if (e) {
+      res.send(e);
+    } else {
+      res.render("blogs", { docs: docs });
+    }
+  });
+});
+
+router.get("/sports", (req, res) => {
+  Blogs.find({ category: "Sports" }, (e, docs) => {
+    if (e) {
+      res.send(e);
+    } else {
+      res.render("blogs", { docs: docs });
+    }
+  });
+});
+
+router.get("/technology", (req, res) => {
+  Blogs.find({ category: "Technology" }, (e, docs) => {
+    if (e) {
+      res.send(e);
+    } else {
+      res.render("blogs", { docs: docs });
+    }
+  });
+});
+
+router.get("/education", (req, res) => {
+  Blogs.find({ category: "Education" }, (e, docs) => {
+    if (e) {
+      res.send(e);
+    } else {
+      res.render("blogs", { docs: docs });
+    }
+  });
+});
+
+router.get("/healthcare", (req, res) => {
+  Blogs.find({ category: "Healthcare" }, (e, docs) => {
+    if (e) {
+      res.send(e);
+    } else {
+      res.render("blogs", { docs: docs });
+    }
+  });
+});
+
+router.get("/other", (req, res) => {
+  Blogs.find({ category: "Other" }, (e, docs) => {
+    if (e) {
+      res.send(e);
+    } else {
+      res.render("blogs", { docs: docs });
+    }
+  });
+});
 
 module.exports = router;
