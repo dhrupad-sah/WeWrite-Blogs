46a90db7907bf4b2259a47b6d1635bd29e245789
Dhrupad <thedhrupad@gmail.com>
2023-01-30 16:42:34 +0530

    adding login google testing 5

--- routes/profile.js
+++ routes/profile.js
@@ -24,21 +24,34 @@ passport.use(
   )
 );
 
+router.get("/login", (req,res)=>
+{
+  res.render("login");
+})
+
 router.get(
   "/auth/google",
   passport.authenticate("google", { scope: ["profile"] })
 );
 
+
+
 router.get('/auth/google/w2', 
   passport.authenticate('google', { failureRedirect: '/profile/login' }),
   function(req, res) {
+    // res.send("done");
     // Successful authentication, redirect home.
     res.redirect('/blogs/politics');
   });
 
-  router.get("/login", (req,res)=>
-  {
-    res.render("login");
-  })
+//  router.get("/auth/google/logout", function(req, res){
+//     if (req.user) {
+//       req.session.destroy();
+//       req.logout();
+//       res.redirect("/"); //redirect back to the frontend home page
+//     }else {
+//       res.send("user is already logged out!");
+//     }
+//   });
 
 module.exports = router;
